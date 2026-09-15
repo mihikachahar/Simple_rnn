470156119f1cad1dff1a4a8e76b684c8d2de8893
mihikachahar <mihikachahar2004@gmail.com>
2026-09-15 19:04:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-!pip install streamlit
+
 # Step 1: Import Libraries and Load the Model
 import numpy as np
 import tensorflow as tf
+import streamlit as st
 from tensorflow.keras.datasets import imdb
 from tensorflow.keras.preprocessing import sequence
 from tensorflow.keras.models import load_model
